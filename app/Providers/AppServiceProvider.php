4a0be80a47b064d1632ea52fda47ee468b0a1c46
Ralf Lenard <capindingralflenrad@gmail.com>
2026-05-19 15:18:03 +0800

    addin url

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+
+        URL::forceScheme('https');
         Inertia::share([
             'flash' => function () {
                 return [

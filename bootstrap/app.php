bbe16a8c7faacce577c2167f47c297db3c5a441b
Ralf Lenard <capindingralflenrad@gmail.com>
2026-05-19 16:46:04 +0800

    rename admin middleware 2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,7 +25,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         $middleware->alias([
-            'admin' => adminMiddleware::class,
+            'admin' => AdminMiddleware::class,
             'staff' => StaffMiddleware::class
         ]);
     })

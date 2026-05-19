3c68b05c6d1b15ba26519a148f055673de1602f2
Ralf Lenard <capindingralflenrad@gmail.com>
2026-05-19 16:38:22 +0800

    Add AdminMiddleware

--- app/Http/Middleware/adminMiddleware.php
+++ app/Http/Middleware/adminMiddleware.php
@@ -4,20 +4,16 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class AdminMiddleware
 {
-    /**
-     * Handle an incoming request.
-     */
     public function handle(Request $request, Closure $next): Response
     {
-        if (!Auth::check() || Auth::user()->usertype !== 'admin') {
-            return redirect('/guest')->with('error', 'Access denied. Admins only.');
+        if (auth()->check() && auth()->user()->role === 'admin') {
+            return $next($request);
         }
 
-        return $next($request);
+        return redirect('/')->with('error', 'Access denied. Admins only.');
     }
 }
